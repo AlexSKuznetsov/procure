891a637dfae37b78a27ae79aef9f7e66faaa7619
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2023-10-22 21:03:08 +0300

    Added manual mass start, small ui impr

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import NextLink from 'next/link';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { usePathname } from 'next/navigation';
+import { Company } from 'prisma/prisma-client';
 import { CompanySelect } from './CompanySelect';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
@@ -23,14 +24,14 @@ export const NavBar = () => {
   return (
     <NavigationMenu.Root className='relative z-[1]'>
       <NavigationMenu.List className='m-0 flex w-screen list-none justify-center space-x-6 bg-slate-50 px-8 py-2 shadow'>
-        {navItems.map((el) => (
-          <NavigationMenu.Item key={el.text}>
-            <NavigationMenu.Link asChild active={pathname === el.link}>
+        {navItems.map(({ text, link }) => (
+          <NavigationMenu.Item key={text}>
+            <NavigationMenu.Link asChild active={pathname === link}>
               <NextLink
                 className='p-2 text-gray-700 hover:bg-blue-100 hover:text-white data-[active]:border-b-4 data-[active]:border-blue-600 data-[active]:text-blue-700'
-                href={el.link}
+                href={link}
               >
-                {el.text}
+                {text}
               </NextLink>
             </NavigationMenu.Link>
           </NavigationMenu.Item>
---
+++ temporal/src/manualStart.ts
@@ -0,0 +1,45 @@
+import { Client, Connection } from '@temporalio/client';
+import { startProcureProcess } from '../../temporal/src/workflow';
+import { PrismaClient } from 'prisma/prisma-client';
+import { v4 as uuidv4 } from 'uuid';
+
+// npx tsx ./temporal/src/manualStart.ts
+
+const startMultipleWorkflows = async (count: number) => {
+  const prismaClient = new PrismaClient();
+  const connection = await Connection.connect({});
+  const client = new Client({ connection });
+  const company = await prismaClient.company.findFirst();
+
+  if (company) {
+    Array(count)
+      .fill({})
+      .forEach(async (_, i) => {
+        try {
+          const lotId = uuidv4();
+          const description = `lorem ipsum`;
+          const result = await client.workflow.start(startProcureProcess, {
+            workflowId: lotId,
+            args: [
+              {
+                name: `Auto lot + ${i}`,
+                description,
+                duration: '60 sec',
+                lotId,
+                companyId: company.id,
+              },
+            ],
+            taskQueue: 'start-procur',
+          });
+
+          console.log('workflow ' + i + ' started: ' + result.workflowId);
+
+          return result.workflowId;
+        } catch (e) {
+          return new Error('Failed to start procure workflow');
+        }
+      });
+  }
+};
+
+startMultipleWorkflows(10);
